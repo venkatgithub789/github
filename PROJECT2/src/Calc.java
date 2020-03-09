50ba6444b4887b5a23768468322515872d047d87
venkata subrahmanyam <venkata subrahmanyam@LAPTOP-K53SDBCJ>
2020-03-10 01:45:34 +0530

    sample  prgs add and calc both updated

---
+++ PROJECT2/src/Calc.java
@@ -0,0 +1,31 @@
+
+public class Calc {
+
+	
+	public void hello()
+	
+	{
+		System.out.println("hello this is a class method");
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		   int a,b;
+		   
+		   a = 20;
+		   b = 5;
+		   
+		   System.out.println(a+b);
+		   System.out.println(a-b);
+		   System.out.println(a*b);
+		   System.out.println(a/b);
+		   System.out.println(a%b);
+		   System.out.println(Math.pow(a, b));
+		
+		   
+		   System.out.println("hello ths is calc ");
+	}
+
+}
---
+++ PROJECT2/src/add.java
@@ -0,0 +1,25 @@
+
+public class add {
+
+	
+	public void add1()
+	{
+	System.out.println("hello this is addition");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	int x, y;	
+	 x = 40;
+	 y = 20;
+		
+		System.out.print(x+y);
+		
+		
+		}
+
+	
+	
+}
+
